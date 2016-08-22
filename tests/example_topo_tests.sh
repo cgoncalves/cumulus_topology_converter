d8745ff4fd516e65fc155d9e1382b45f13869d54
Eric Pulvino <ericpulvino@gmail.com>
2016-08-22 17:13:40 -0400

    FIRST implementation of auto mgmt network builder. Added a default subnet for MGMT server if not defined. Added simple Bash tests.

---
+++ tests/example_topo_tests.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+check_state(){
+if [ "$?" != "0" ]; then
+    echo "ERROR Test Failed!"
+    exit 1
+fi
+}
+
+echo "####################################"
+echo "############# TEST 1: 2switch.dot Vbox ###############"
+echo "####################################"
+python ./topology_converter.py ./examples/2switch.dot && vagrant status
+
+check_state
+
+echo "####################################"
+echo "############# TEST 2: 2switch.dot libvirt ###############"
+echo "####################################"
+python ./topology_converter.py ./examples/2switch.dot -p libvirt && vagrant status
+
+check_state
+
+echo "####################################"
+echo "############# TEST 3: 1switch_1server.dot Vbox ###############"
+echo "####################################"
+python ./topology_converter.py ./examples/1switch_1server.dot && vagrant status
+
+check_state
+
+echo "####################################"
+echo "############# TEST 4: 1switch_1server.dot libvirt ###############"
+echo "####################################"
+python ./topology_converter.py ./examples/1switch_1server.dot -p libvirt && vagrant status
+
+check_state
+
+echo "####################################"
+echo "############# TEST 5: 3switch_circular.dot Vbox ###############"
+echo "####################################"
+python ./topology_converter.py ./examples/3switch_circular.dot && vagrant status
+
+check_state
+
+echo "####################################"
+echo "############# TEST 6: 3switch_circular.dot libvirt ###############"
+echo "####################################"
+python ./topology_converter.py ./examples/3switch_circular.dot -p libvirt && vagrant status
+
+check_state
+
+echo "####################################"
+echo "############# TEST 7: customer_topology.dot Vbox ###############"
+echo "####################################"
+python ./topology_converter.py ./examples/customer_topology.dot && vagrant status
+
+check_state
+
+echo "####################################"
+echo "############# TEST 8: customer_topology.dot libvirt ###############"
+echo "####################################"
+python ./topology_converter.py ./examples/customer_topology.dot -p libvirt && vagrant status
+
+check_state
+
+echo "####################################"
+echo "############# TEST 9: 2switch_tun_ip.dot libvirt ###############"
+echo "####################################"
+python ./topology_converter.py ./examples/2switch_tun_ip.dot -p libvirt && vagrant status
+
+check_state
