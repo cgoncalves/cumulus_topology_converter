21eda07f5dc42642919ec25db03c56c138388b28
Michael Robertson <mrobertson@cumulusnetworks.com>
2020-03-30 13:51:18 -0400

    Pull useful functions into a lib module so they can be reused

---
+++ lib/tc_config.py
@@ -0,0 +1,37 @@
+"""
+The TcConfig module represents a configuration set needed for running Topology Converter
+"""
+import sys
+
+class TcConfig:
+    """
+    Provides configuration options with defaults for TopologyConverter. An instance of this class
+    can be passed around to various functions to maintain a standard configuration across modules.
+    """
+    def __init__(self, **kwargs):
+        clean_kwargs = {k:v for k, v in kwargs.items() if v is not None} # remove null kwargs
+
+        self.arg_string = clean_kwargs.get('arg_string', ' '.join(sys.argv))
+        self.create_mgmt_configs_only = clean_kwargs.get('create_mgmt_configs_only', False)
+        self.create_mgmt_device = clean_kwargs.get('create_mgmt_device', False)
+        self.create_mgmt_network = clean_kwargs.get('create_mgmt_network', False)
+        self.customer = clean_kwargs.get('customer', None)
+        self.display_datastructures = clean_kwargs.get('display_datastructures', False)
+        self.function_group = clean_kwargs.get('function_group', {})
+        self.generate_ansible_hostfile = clean_kwargs.get('generate_ansible_hostfile', False)
+        self.libvirt_prefix = clean_kwargs.get('libvirt_prefix', None)
+        self.mac_map = {}
+        self.network_functions = clean_kwargs.get('network_functions',
+                                                  ['oob-switch', 'internet', 'exit', 'superspine',
+                                                   'spine', 'leaf', 'tor'])
+        self.parser = clean_kwargs.get('parser', None)
+        self.port_gap = clean_kwargs.get('port_gap', 1000)
+        self.provider = clean_kwargs.get('provider', 'virtualbox')
+        self.script_storage = clean_kwargs.get('script_storage', './helper_scripts')
+        self.start_mac = clean_kwargs.get('start_mac', '443839000000')
+        self.start_port = clean_kwargs.get('start_port', 8000)
+        self.synced_folder = clean_kwargs.get('synced_folder', False)
+        self.tunnel_ip = clean_kwargs.get('tunnel_ip', None)
+        self.total_memory = clean_kwargs.get('total_memory', 0)
+        self.vagrant = clean_kwargs.get('vagrant', 'eth0')
+        self.verbose = clean_kwargs.get('verbose', 0)
---
+++ lib/tc_error.py
@@ -0,0 +1,5 @@
+"""
+Topology Converter-specific exceptions
+"""
+class TcError(BaseException):
+    """ Provides a base exception that more specific errors can inherit from """
